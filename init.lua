c591f2b69d6c38168298a4cece299093e7b86a51
Timur Zakhoda <timurzakhoda@gmail.com>
2024-07-04 14:16:48 +0700

    lsp/fmt for yaml

--- init.lua
+++ init.lua
@@ -540,6 +540,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
+        yamlls = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -571,6 +572,8 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'gopls',
+        'sqls',
+        'yamlfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -619,6 +622,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         go = { 'gofmt', 'goimports' },
+        yaml = { 'yamlfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
